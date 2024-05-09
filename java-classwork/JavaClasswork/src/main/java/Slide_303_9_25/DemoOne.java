444244c993d22d0e6b21c5023dfa08a8e5f43e90
DESKTOP-DHHSBN5\Anson. R <mayli.fiaccone@gmail.com>
2024-05-09 08:36:08 -0400

    homework push 5/9

---
+++ java-classwork/JavaClasswork/src/main/java/Slide_303_9_25/DemoOne.java
@@ -0,0 +1,7 @@
+package Slide_303_9_25;
+
+public class DemoOne {
+    static int var1 = 20;
+    static String var2 = "perscholas";
+    int var3 = 100;
+}
--- java-classwork/JavaClasswork/src/main/java/coffeeshop/Main.java
+++ java-classwork/JavaClasswork/src/main/java/coffeeshop/Main.java
@@ -7,19 +7,35 @@ import java.util.Scanner;
 public class Main {
 
     private CoffeeShop coffeeShop;
-    private Scanner scanner = new Scanner(System.in);
+    private Scanner scanner;
+
+    public Main() {
+        scanner = new Scanner(System.in);
+
+    }
 
     public void printMenu() {
         System.out.println("0) Exit");
         System.out.println("1) Print the list of products");
         System.out.println("2) Order a product");
         System.out.println("3) Checkout");
+
+        System.out.println("Enter your choice ");
+        int option = scanner.nextInt();
+
     }
 
     public void runCoffeeShop() {
         coffeeShop = new CoffeeShop();
         coffeeShop.initProducts();
         coffeeShop.printProducts();
+
+        while(true) {
+            boolean value = true;
+
+
+
+        }
     }
 
     // we only have 1 public static void main for the whole coffee shop project
