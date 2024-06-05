0f1aea749f6694bc0329f767541dd5515e3032c1
DESKTOP-DHHSBN5\Anson. R <mayli.fiaccone@gmail.com>
2024-06-05 18:12:51 -0400

    cleaned up pseudocode

--- module-305/M305-JDBC/src/main/java/org/example/database/dao/CustomerDAO.java
+++ module-305/M305-JDBC/src/main/java/org/example/database/dao/CustomerDAO.java
@@ -12,25 +12,18 @@ import java.util.List;
 public class CustomerDAO {
 
     public List<Customer> findByCustomerName (String customerName) {
-        // these 2 lines of code prepare the hibernate session for use
+
         SessionFactory factory = new Configuration().configure().buildSessionFactory();
         Session session = factory.openSession();
 
-        // JPA Query - the syntax is slightly different than regular SQL
-        // SQL - "select * from employees e where e.firstname = ?"
         String hql = "SELECT c FROM Customer c where c.customerName = :customername";
 
-        // this typed query is how hibernate knows what kind of object of fill up with the query results
         TypedQuery<Customer> query = session.createQuery(hql,Customer.class);
-        // this is similar to the prepared statement, we are going to set the value in the query :firstname
-        // to the value passed into the function
+
         query.setParameter("customername", customerName);
 
-        // when we know we are getting 0 or more records we use getResultList
         List<Customer> result = query.getResultList();
 
-        // finally we close the hibernate session so that it can release the resources its holding
-        // specifically the connection pool and close the transaction
         session.close();
 
         return result;
@@ -38,25 +31,19 @@ public class CustomerDAO {
 
 
     public List<Customer> findByContactFirstName(String contactFirstName) {
-        // these 2 lines of code prepare the hibernate session for use
+
         SessionFactory factory = new Configuration().configure().buildSessionFactory();
         Session session = factory.openSession();
 
-        // JPA Query - the syntax is slightly different than regular SQL
-        // SQL - "select * from employees e where e.firstname = ?"
         String hql = "SELECT c FROM Customer c where c.contactFirstName = :firstname";
 
-        // this typed query is how hibernate knows what kind of object of fill up with the query results
+
         TypedQuery<Customer> query = session.createQuery(hql,Customer.class);
-        // this is similar to the prepared statement, we are going to set the value in the query :firstname
-        // to the value passed into the function
+
         query.setParameter("firstname", contactFirstName);
 
-        // when we know we are getting 0 or more records we use getResultList
         List<Customer> result = query.getResultList();
 
-        // finally we close the hibernate session so that it can release the resources its holding
-        // specifically the connection pool and close the transaction
         session.close();
 
         return result;
@@ -96,7 +83,7 @@ public class CustomerDAO {
         Session session = factory.openSession();
         session.getTransaction().begin();
 
-        // this is the only line that changed
+
         session.delete(customer);
 
         session.getTransaction().commit();
@@ -106,17 +93,14 @@ public class CustomerDAO {
 
 
     public Customer findById (Integer id) {
-        // these 2 lines of code prepare the hibernate session for use
+
         SessionFactory factory = new Configuration().configure().buildSessionFactory();
         Session session = factory.openSession();
 
         String hql = "SELECT c FROM Customer c where c.id = :id";
-
-
-        // this typed query is how hibernate knows what kind of object of fill up with the query results
+        
         TypedQuery<Customer> query = session.createQuery(hql,Customer.class);
-        // this is similar to the prepared statement, we are going to set the value in the query :firstname
-        // to the value passed into the function
+
         query.setParameter("id", id);
 
         try {
@@ -126,8 +110,7 @@ public class CustomerDAO {
             return null;
         } finally {
 
-            // finally we close the hibernate session so that it can release the resources its holding
-            // specifically the connection pool and close the transaction
+
             session.close();
         }
     }
