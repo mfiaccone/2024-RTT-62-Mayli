2bdf8bfe4f9bf49f902a57e14ccfc7b91b00ccd2
DESKTOP-DHHSBN5\Anson. R <mayli.fiaccone@gmail.com>
2024-06-25 17:43:02 -0400

    form validation and buttonclick classwork

---
+++ module-308/class-examples/buttonclick.js
@@ -0,0 +1,12 @@
+function changeText() {
+    const p = document.querySelector('p');
+    p.textContent = "Changed because of an event handler property.";
+    
+    const button = document.querySelector('button');
+    button.textContent = "I was clicked";
+
+  }
+
+  function gotoLookup() {
+    window.location.href = '/lookup.html'
+  }
