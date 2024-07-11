093061931f84acd89c47f7aa7b52c5c9f6cc331d
DESKTOP-DHHSBN5\Anson. R <mayli.fiaccone@gmail.com>
2024-07-11 10:07:11 -0400

    created product entities and some basic connections

--- spring-boot/src/main/java/com/example/springboot/controller/IndexController.java
+++ spring-boot/src/main/java/com/example/springboot/controller/IndexController.java
@@ -1,16 +1,30 @@
 package com.example.springboot.controller;
 
+import com.example.springboot.database.dao.ProductDAO;
+import com.example.springboot.database.entity.Product;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class IndexController {
 
+
+    @Autowired
+    private ProductDAO productDao;
+
     @GetMapping("/")
-    public ModelAndView index() {
+    public ModelAndView index(@RequestParam Integer id) {
         // this function is for the home page of the website which is expressed as just a plain slash "/"
         ModelAndView response = new ModelAndView("index");
+
+        Product product = productDao.findById(id);
+        response.addObject("productKey", product);
+
+        response.addObject("'message", "Hello World!");
+
 
         return response;
     }
--- spring-boot/src/main/webapp/WEB-INF/jsp/index.jsp
+++ spring-boot/src/main/webapp/WEB-INF/jsp/index.jsp
@@ -3,4 +3,11 @@
 <h1>This is my first jsp page</h1>
 <a href="/page-url">My second page</a>
 
+<table class = table>
+    <tr><td>id</td><td>${productKey.id}</td></tr>
+    <tr><td>code</td><td>${productKey.productCode}</td></tr>
+    <tr><td>name</td><td>${productKey.productName}</td></tr>
+    <tr><td>description</td><td>${productKey.productDescription}</td></tr>
+</table>
+
 <jsp:include page="include/footer.jsp"/>
